85bef4e6f967c318823c639ae2af5f90f7f48ea1
kbaba1001 <kbaba1001@users.noreply.github.com>
2017-04-07 00:13:16 +0900

    User custom attributes

--- lib/geeami/entities/user.rb
+++ lib/geeami/entities/user.rb
@@ -1,2 +1,11 @@
 class User < Hanami::Entity
+  EMAIL_FORMAT = /\s*([^@\\s]{1,64})@((?:[-\p{L}\d]+\.)+\p{L}{2,})\s*/i
+
+  attributes do
+    attribute :id, Types::Int
+    attribute :email, Types::String.constrained(format: EMAIL_FORMAT)
+    attribute :password_digest, Types::String
+    attribute :created_at, Types::Time
+    attribute :updated_at, Types::Time
+  end
 end
--- spec/geeami/entities/user_spec.rb
+++ spec/geeami/entities/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  # place your tests here
+  it '妥当なデータを入れるケース' do
+    user = User.new(email: 'user1@example.com', password_digest: 'xxxxxxxxxx')
+
+    assert { user.email == 'user1@example.com' }
+    assert { user.password_digest == 'xxxxxxxxxx' }
+  end
+
+  describe 'invalid' do
+    it 'emailのフォーマットが不正であるケース' do
+      proc {
+        User.new(email: 'user1example.com')
+      }.must_raise TypeError
+    end
+  end
 end
