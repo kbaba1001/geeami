593ecebcac1732809c18b06310a59b7d3417a77a
kbaba1001 <kbaba1001@users.noreply.github.com>
2017-05-02 15:49:30 +0900

    remove trash

--- spec/web/controllers/users/create_spec.rb
+++ spec/web/controllers/users/create_spec.rb
@@ -3,10 +3,20 @@ require_relative '../../../../apps/web/controllers/users/create'
 
 describe Web::Controllers::Users::Create do
   let(:action) { Web::Controllers::Users::Create.new }
-  let(:params) { Hash[] }
+  let(:params) {
+    {
+      user: {
+        email: 'user1@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+    }
+  }
 
   it 'is successful' do
     response = action.call(params)
-    response[0].must_equal 200
+
+    assert { action.user.email == 'user1@example.com' }
+    assert { Auth::Password.compare?(action.user.password_digest, 'password')  }
   end
 end
