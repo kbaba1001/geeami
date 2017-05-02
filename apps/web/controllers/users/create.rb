7b58afc0e0f8b96241dfb9e29d48f9f7deb19f79
kbaba1001 <kbaba1001@users.noreply.github.com>
2017-05-02 10:19:11 +0900

    password hashing

--- apps/web/controllers/users/create.rb
+++ apps/web/controllers/users/create.rb
@@ -2,8 +2,33 @@ module Web::Controllers::Users
   class Create
     include Web::Action
 
+    expose :user
+
+    params do
+      required(:user).schema do
+        required(:email).filled(:str?)
+        required(:password).filled(:str?)
+        required(:password_confirmation).filled(:str?)
+      end
+    end
+
     def call(params)
-      p params
+      if params.valid?
+        @user = sign_up_user
+
+        redirect_to '/'
+      else
+        render new
+      end
+    end
+
+    def sign_up_user
+      password_digest = Auth::Password.hashing(params[:user][:password])
+
+      UserRepository.new.create(
+        email: params[:user][:email],
+        password_digest: password_digest
+      )
     end
   end
 end
