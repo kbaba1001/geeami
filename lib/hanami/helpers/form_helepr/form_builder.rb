296ae3d9a7787e353cafc0e09af844b5d1db3403
kbaba1001 <kbaba1001@users.noreply.github.com>
2017-05-02 18:29:48 +0900

    define error_field

---
+++ lib/hanami/helpers/form_helepr/form_builder.rb
@@ -0,0 +1,21 @@
+require 'hanami/helpers/form_helper/form_builder.rb'
+
+# モンキーパッチ
+module Hanami
+  module Helpers
+    module FormHelper
+      class FormBuilder
+        def error_field(name, attributes = {}, &blk)
+          return if params.valid?
+
+          errors = params.errors[@name][name]
+          return if errors.empty?
+
+          attrs = {class: 'error-field'}.merge(attributes)
+
+          div errors.join(', '), attrs, &blk
+        end
+      end
+    end
+  end
+end
