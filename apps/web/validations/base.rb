5e1796d1d00e9479509f7ce8a4944caafe0ff619
kbaba1001 <kbaba1001@users.noreply.github.com>
2017-05-10 01:41:10 +0900

    predicateにmessageを定義
    yamlからよんでくれなかった...

--- apps/web/validations/base.rb
+++ apps/web/validations/base.rb
@@ -1,12 +1,13 @@
 module Web::Validations
   class Base < Hanami::Action::Params
-    messages_path Hanami.root.join('config/locales/ja.yml')
+    messages_path 'config/locales/ja.yml'
 
-    predicate(:email?) do |value|
+    # FIXME yamlからメッセージを取れないっぽい
+    predicate(:email?, message: "メールアドレスのフォーマットが不正です") do |value|
       /\A\s*([^@\s]{1,64})@((?:[-\p{L}\d]+\.)+\p{L}{2,})\s*\z/i === value
     end
 
-    predicate(:unique?) do |klass, value|
+    predicate(:unique?, message: 'メールアドレスは登録済みです') do |klass, value|
       # TODO SQLの発行はRepositoryに任せたほうがよい。あと exists の方が良さそう。
       !! klass.new.send(:users).send(:where, email: 'kbaba1001@example.com').first
     end
