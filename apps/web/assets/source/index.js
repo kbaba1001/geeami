2be312a6e32c15abc98b9fed865527e349c5ccfe
kbaba1001 <kbaba1001@users.noreply.github.com>
2017-05-13 21:36:15 +0900

    vueがうまく動いてくれない
    buildはできるが、browserで見たときに el の要素がコメントになってしまう。
    stackoverflow によると render がうまくできてないようなので、
    vue-loader を入れてやるほうが良いのかもしれない

--- apps/web/assets/source/index.js
+++ apps/web/assets/source/index.js
@@ -1,2 +1,20 @@
-import worksCheck from "./worksCheck";
-worksCheck.print();
+// import worksCheck from "./worksCheck";
+// worksCheck.print();
+
+// http://stackoverflow.com/questions/42124812/component-rendered-as-comment-in-vuejs-jspm
+// TODO vue-loader 入れないとダメっぽい
+import Vue from 'vue/dist/vue.esm.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const vm = new Vue({
+    el: '#app1',
+    render: function(createElement) {
+      return createElement('div', {}, this.message);
+    },
+    data: {
+      msg: 'Hello Vue!'
+    }
+  });
+
+  console.log(vm)
+})
--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,12 @@
 var path = require("path");
+var webpack = require('webpack');
 
 module.exports = {
   context: path.resolve(__dirname, "apps/web/assets/source"),
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "apps/web/assets/javascripts/"),
-    filename: "output.js"
+    filename: "build.js"
   },
   module: {
     loaders: [
@@ -16,5 +17,17 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ],
   devtool: "inline-source-map"
 };
