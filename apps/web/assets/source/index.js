adad54c41d1fb0c00bbff937417f6ebd7cb44fd2
kbaba1001 <kbaba1001@users.noreply.github.com>
2017-05-23 21:12:11 +0900

    wip

--- apps/web/assets/source/index.js
+++ apps/web/assets/source/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
-import App from './App.vue'
+// import Vuetify from 'vuetify';
+//
+// Vue.use(Vuetify);
+
+import App from './App.vue';
 
 // document.ready
 document.addEventListener('DOMContentLoaded', () => {
